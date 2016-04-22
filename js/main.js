40deb3d86bc8c00e95f55e96840edc7efa9a0783
Juan <jgtaruc@gmail.com>
2016-04-22 14:40:06 -0700

    skeleton of frontpage; needs more styling and content

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function show_nm(){
+	var form = document.querySelector("#dropdown-form");
+	if(form.style.display == "none"){
+		form.style.display = "flex";
+	}
+	else{
+		form.style.display = "none";
+	}
+}
+
+
